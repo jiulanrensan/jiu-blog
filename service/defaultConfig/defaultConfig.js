edcc56f080e2f8d6145aa7a8965c7a7500e9f366
lijiacheng <839492148@qq.com>
2020-07-27 22:15:48 +0800

    style(service): 新建service目录存放默认配置文件

---
+++ service/defaultConfig/defaultConfig.js
@@ -0,0 +1,7 @@
+export const headerContent = [
+  {'label': '首页', 'value': 'home'},
+  {'label': '沸点', 'value': 'hot'},
+  {'label': '话题', 'value': 'topic'},
+  {'label': '小册', 'value': 'book'},
+  {'label': '活动', 'value': 'activity'}
+]
