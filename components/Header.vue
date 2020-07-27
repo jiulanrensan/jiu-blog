497eac96eedfa66745fa46aa2448e2c95c289b32
lijiacheng <839492148@qq.com>
2020-07-27 22:52:31 +0800

    feature(Header.vue): 完善导航栏样式

--- components/Header.vue
+++ components/Header.vue
@@ -1,41 +1,61 @@
 <template>
 	<header class="jiu_header">
-		<div class="jiu_header-img">
-			<nuxt-link to="/">
-				<img src="@/assets/img/juejin.svg" alt="">
-			</nuxt-link>
+		<div class="jiu_header_box">
+			<div class="jiu_header-img">
+				<nuxt-link to="/">
+					<img src="@/assets/img/juejin.svg" alt="">
+				</nuxt-link>
+			</div>
+			<nav class="jiu_header-nav">
+				<ul>
+					<li v-for="item in navItems" :key="item.value">{{item.label}}</li>
+				</ul>
+			</nav>
 		</div>
-		<nav class="jiu_header-nav">
-			<ul v-for="item in navItems" :key="item.value">
-				<li>{{item.label}}</li>
-			</ul>
-		</nav>
 	</header>
 </template>
 
 <script>
+import {headerContent} from '@/service/defaultConfig'
 export default {
 	data () {
 		return {
-			navItems: [
-				{'label': '首页', 'value': 'home'},
-				{'label': '沸点', 'value': 'hot'},
-				{'label': '话题', 'value': 'topic'},
-				{'label': '小册', 'value': 'book'},
-				{'label': '活动', 'value': 'activity'}
-			]
+			navItems: headerContent
 		}
 	}
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '@/assets/css/common.scss';
 .jiu_header{
-	display: flex;
-}
-.jiu_header-nav{
-	display: flex;
-	justify-content: center;
-	flex-wrap: nowrap;
+	background-color: #fff;
+
+	.jiu_header_box{
+		@include flexCenter(left);
+		width: 70%;
+		height: 60px;
+		margin: auto;
+
+		.jiu_header-img{
+			@include flexCenter(center);
+		}
+
+		.jiu_header-nav{
+			ul{
+				height: 100%;
+				margin-left: 10px;
+				@include flexCenter(center);
+
+				li{
+					padding: 10px;
+					&:hover{
+						cursor: pointer;
+						color: $--primary-color;
+					}
+				}
+			}
+		}
+	}
 }
 </style>
